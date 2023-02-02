1c5645cb65a446f5f568cbbc9802a1d038bd5616
Jom Gapuz <bark@jomgapuz.com>
2023-02-03 07:51:44 +0800

    feat: make EventBusFactory functional

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
 const EventBusFactory = () => {
+    /**
+     * @type {Record<string, Set<((payload: any) => any)> | undefined>}
+     */
     const listeners = {};
 
+    /**
+     * Publish message
+     * @param {string} event
+     * @param {any} payload
+     */
     const fire = (event, payload) => {
         // publish message here
+        const eventListeners = listeners[event]
+
+        return eventListeners ? [...eventListeners].map((listener) => listener(payload)) : []
     };
 
+    /**
+     * Subscribe to a message
+     * @param {string} event 
+     * @param {(payload: any) => any} listener
+     */
     const listen = (event, listener) => {
-        // subscribe a message
+        if (!(event in listeners)) {
+            listeners[event] = new Set([listener])
+        } else {
+            listeners[event].add(listener)
+        }
     };
 
+
+    /**
+     * Unsubscribe from an event
+     * @param {string} event 
+     * @param {(payload: any) => any} removeListener
+     */
     const unsubscribe = (event, removeListener) => {
-        // unsubscribe from event
+        if ((event in listeners)) {
+            listeners[event].delete(removeListener)
+        }
     };
 
     return {
